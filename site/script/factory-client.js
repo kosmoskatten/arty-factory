c50a0c8ba1d9c944e39b09664bd25b7555f4f422
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-12-08 23:43:09 +0100

    Alternative way to download.

--- site/script/factory-client.js
+++ site/script/factory-client.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     createLayout();
-    var $e1=createEntry("e1");
-    var $e2=createEntry("e2");
+    var $e1=createEntry("/storage/buildR1B65.tar.gz");
+    var $e2=createEntry2("buildR1B66.tar.gz");
 
     $("#box").append($e1);
     $("#box").append($e2);
 
-    $("#box").on("click", "img.download", function() {
-        alert($(this).attr("id"));
-    });
+    //$("#box").on("click", "img.download", function() {
+    //    alert($(this).attr("id"));
+    //});
 });
 
 function createLayout() {
@@ -23,5 +23,17 @@ function createEntry(name) {
                                src: "/image/download.png",
                                title: "Download " + name});
     $entry.append($download);
+    return $entry;
+}
+
+function createEntry2(name) {
+    var $icon=$("<img/>", {class: "download",
+                           src: "/image/download.png",
+                           title: "Download " + name});
+    var $link=$("<a/>", {href: "/storage/" + name,
+                         download: name});
+    var $entry=$("<div/>", {class: "entry"});
+    $link.append($icon);
+    $entry.append($link);
     return $entry;
 }
