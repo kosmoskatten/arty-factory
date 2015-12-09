9a8f815329e8df7a169040ba97a9a61ad8495c70
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-12-09 13:24:37 +0100

    Minimum functionality.

--- site/script/factory-client.js
+++ site/script/factory-client.js
@@ -1,39 +1,50 @@
 $(document).ready(function() {
-    createLayout();
-    var $e1=createEntry("/storage/buildR1B65.tar.gz");
-    var $e2=createEntry2("buildR1B66.tar.gz");
+    $layout=createLayout();
+    $("body").append($layout);
 
-    $("#box").append($e1);
-    $("#box").append($e2);
-
-    //$("#box").on("click", "img.download", function() {
-    //    alert($(this).attr("id"));
-    //});
+    updateFileList();
+    $("#update").click(function() {
+        $("#box").empty();
+        updateFileList();
+    });
 });
 
+function updateFileList() {
+    $.get("/storage", function(files) {
+        files.forEach(function(file) {
+            var $entry=createEntry(file);
+            $("#box").append($entry);
+        });
+    }, "json");
+}
+
 function createLayout() {
+    var $layout=$("<div/>", {id: "layout", class: "layout"});
+    var $update=$("<button/>", {id: "update",
+                                class: "update",
+                                text: "Update Arty-Factory List"});
     var $box=$("<div/>", {id: "box", class: "box"});
-    $("body").append($box);
+    var $footer=$("<div/>", {id: "footer", class: "footer"});
+    var $logo=$("<h1/>", {class: "footer", text: "Demo Arty-Factory"});
+
+    $footer.append($logo);
+    $layout.append($update);
+    $layout.append($box);
+    $layout.append($footer);
+
+    return $layout;
 }
 
-function createEntry(name) {
-    var $entry=$("<div/>", {class: "entry"});
-    var $download=$("<img/>", {id: name, 
-                               class: "download",
-                               src: "/image/download.png",
-                               title: "Download " + name});
-    $entry.append($download);
-    return $entry;
-}
-
-function createEntry2(name) {
+function createEntry(file) {
+    var $label=$("<p/>", {class: "entry", text: file.fullPath}); 
     var $icon=$("<img/>", {class: "download",
                            src: "/image/download.png",
-                           title: "Download " + name});
-    var $link=$("<a/>", {href: "/storage/" + name,
-                         download: name});
+                           title: "Download " + file.name});
+    var $link=$("<a/>", {href: file.fullPath,
+                         download: file.name});
     var $entry=$("<div/>", {class: "entry"});
     $link.append($icon);
+    $entry.append($label);
     $entry.append($link);
     return $entry;
 }
--- src/Server.hs
+++ src/Server.hs
@@ -1,12 +1,57 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main
-    ( main
+    ( Artyfact (..)
+    , main
     ) where
 
+import Control.Monad (filterM)
+import Data.Aeson (ToJSON)
+import Data.Text (Text)
+import GHC.Generics (Generic)
 import Network.Hive
+import System.Directory (getDirectoryContents, doesFileExist)
+
+import qualified Data.Text as T
+
+data Artyfact
+    = Artyfact
+        { name     :: !Text
+        , fullPath :: !Text
+        }
+    deriving (Generic, Show)
+
+instance ToJSON Artyfact
 
 main :: IO ()
 main =
     hive defaultHiveConfig $ do
         get `accepts` Anything
             `handledBy` respondFile "site/index.html"
+
+        get </> "storage" 
+            `accepts` Anything
+            `handledBy` do
+                files <- liftIO getStorageFiles
+                respondJSON $ map toArtyfact files
+
         defaultRoute `handledBy` serveDirectory "site"
+
+toArtyfact :: FilePath -> Artyfact
+toArtyfact file = 
+    Artyfact { name     = T.pack file
+             , fullPath = T.pack $ storageUrlPrefix `mappend` file 
+             }
+
+getStorageFiles :: IO [FilePath]
+getStorageFiles =
+    filterM existingFile =<< getDirectoryContents storageDir
+    where
+        existingFile :: FilePath -> IO Bool
+        existingFile fp = doesFileExist $ storageDir `mappend` fp
+
+storageUrlPrefix :: FilePath
+storageUrlPrefix = "/storage/"
+
+storageDir :: FilePath
+storageDir = "site/storage/"
