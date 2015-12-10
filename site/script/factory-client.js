a5841e57dae1c4326acc19195341d0f43af533dd
Patrik Sandahl <patrik.sandahl@ericsson.com>
2015-12-10 10:39:06 +0100

    Better layout.

--- site/script/factory-client.js
+++ site/script/factory-client.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
+    // Create the layout and add it to the body.
     $layout=createLayout();
     $("body").append($layout);
 
+    // Initial update of the file list.
     updateFileList();
+
+    // Handle button click events.
     $("#update").click(function() {
-        $("#box").empty();
+        $("#container").empty();
         updateFileList();
     });
 });
@@ -13,7 +17,7 @@ function updateFileList() {
     $.get("/storage", function(files) {
         files.forEach(function(file) {
             var $entry=createEntry(file);
-            $("#box").append($entry);
+            $("#container").append($entry);
         });
     }, "json");
 }
@@ -23,21 +27,41 @@ function createLayout() {
                  "right click the arrow (Save link as ...). Unpack, " +
                  "tar xvfz <file>.tar.gz, cd into the directory and " +
                  "run ./run-ccm.sh"
-    var $layout=$("<div/>", {id: "layout", class: "layout"});
+
+    // Create the layout taking the complete browser window.
+    var $layout=$("<div/>", {class: "layout"});
+
+    // Create the centered box containing the application widgets.
+    var $box=$("<div/>", {class: "box"});
+
+    // Create widgets. The update button.
     var $update=$("<button/>", {id: "update",
                                 class: "update",
                                 text: "Update Arty-Factory List"});
-    var $box=$("<div/>", {id: "box", class: "box"});
+
+
+    // The container to hold entries.
+    var $container=$("<div/>", {id: "container", class: "container"});
+
+    // The footer.
     var $footer=$("<div/>", {id: "footer", class: "footer"});
+
+    // Stuff inside the footer.
     var $logo=$("<h1/>", {class: "footer", text: "Demo Arty-Factory"});
     var $info=$("<p/>", {class: "footer",
                         text: infoText});
-
+    
+    // Append to footer.
     $footer.append($logo);
     $footer.append($info);
-    $layout.append($update);
+
+    // Append to box.
+    $box.append($update);
+    $box.append($container);
+    $box.append($footer);
+
+    // Append box to layout.
     $layout.append($box);
-    $layout.append($footer);
 
     return $layout;
 }
